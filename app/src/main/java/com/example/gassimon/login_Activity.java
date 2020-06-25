a8de630ed9cc2f2aaa092cb6e0f7073f35477c57
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2020-06-25 06:26:00 +0100

    Activity Menu para las operaciones del Crud

--- app/src/main/java/com/example/gassimon/login_Activity.java
+++ app/src/main/java/com/example/gassimon/login_Activity.java
@@ -2,13 +2,23 @@ package com.example.gassimon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class login_Activity extends AppCompatActivity {
-
+public class login_Activity extends AppCompatActivity implements View.OnClickListener {
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+        login = findViewById(R.id.btn_login);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(getApplicationContext(),Menu_Activity.class));
     }
 }
