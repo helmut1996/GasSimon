4616e62d7ed2780a9265af1564338287a2b4415c
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2020-06-25 04:41:47 +0100

    Activity login agregada y icono de la App

--- app/src/main/java/com/example/gassimon/MainActivity.java
+++ app/src/main/java/com/example/gassimon/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.gassimon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        entrar = findViewById(R.id.Entrar);
+        entrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(getApplicationContext(),login_Activity.class));
     }
 }
