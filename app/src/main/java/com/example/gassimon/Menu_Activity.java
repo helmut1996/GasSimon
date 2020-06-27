749084ac9cd0a8dc20573a883043d737d3958e72
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2020-06-27 06:17:47 +0100

    modificando el diseño y agregando menu overflow

--- app/src/main/java/com/example/gassimon/Menu_Activity.java
+++ app/src/main/java/com/example/gassimon/Menu_Activity.java
@@ -2,7 +2,12 @@ package com.example.gassimon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Menu_Activity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class Menu_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_);
+
+    }
+    //metodo pata mostar y ocultar el menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
+    //metodo para asignar las funciones correspondientes a las opciones
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.menu_peerfil)
+        {
+            startActivity( new Intent(getApplicationContext(),PerfilActivity.class));
+        }else if(id == R.id.menu_secion){
+            Toast.makeText(this, "Cerrar sesion!!!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/gassimon/login_Activity.java
+++ app/src/main/java/com/example/gassimon/login_Activity.java
@@ -2,23 +2,35 @@ package com.example.gassimon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class login_Activity extends AppCompatActivity implements View.OnClickListener {
-    private Button login;
+    private Button login,registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
         login = findViewById(R.id.btn_login);
+        registrar = findViewById(R.id.btn_registrar);
         login.setOnClickListener(this);
+        registrar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        startActivity(new Intent(getApplicationContext(),Menu_Activity.class));
+        switch (view.getId()){
+            case R.id.btn_login:
+                startActivity(new Intent(getApplicationContext(),Menu_Activity.class));
+                break;
+            case R.id.btn_registrar:
+                startActivity(new Intent(getApplicationContext(), Registrar_Activity.class));
+                break;
+        }
+
     }
 }
+
