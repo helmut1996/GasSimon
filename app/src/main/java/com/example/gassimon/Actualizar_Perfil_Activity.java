8bd6664b61e1ba17ae2a1cfb74b49534882e8010
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2020-06-28 05:55:47 +0100

    Diseño Completado del  perfil del usuario

---
+++ app/src/main/java/com/example/gassimon/Actualizar_Perfil_Activity.java
@@ -0,0 +1,14 @@
+package com.example.gassimon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Actualizar_Perfil_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar__perfil_);
+    }
+}
